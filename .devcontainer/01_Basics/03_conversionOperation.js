ac5050125b84b97961ddbb3865e36791c3c4e165
Lokesh-Sampath <113461748+Lokesh-Sampath@users.noreply.github.com>
2023-10-28 16:09:38 +0000

    coversions

--- .devcontainer/01_Basics/03_conversionOperation.js
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -11,9 +11,32 @@ let isNumber  = Number(score)
 console.log(isNumber);
 console.log(typeof(isNumber)); */
 
-let someValue = "Lokesh"
+/*let someValue = "Lokesh"
 console.log(typeof(someValue));
 
 let converting = Number(someValue)
 console.log(converting);
-console.log(typeof(converting));
+console.log(typeof(converting));*/
+
+
+//***************OPERAIONS************//
+
+
+/*let value = 3
+let negValue = -value // converts the value to negative of the original value
+console.log(negValue); */
+
+// string concatenation
+
+/*let str1 = "Lokesh"
+let str2 = " Hello"
+let str3 =  str2+str1
+
+console.log(str1+str2);
+console.log(str3);*/
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
