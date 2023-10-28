66e23533369c2e4b330a0c3e16d8d3c57ed3c78b
Lokesh-Sampath <113461748+Lokesh-Sampath@users.noreply.github.com>
2023-10-28 15:50:13 +0000

    Type conversion

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// type coversion basically 
+
+// "11"=>11
+//"11abc"=> NaN
+// true=>1  false=>0
+
+/*let score = "33"
+console.log(typeof(score));
+
+let isNumber  = Number(score)
+console.log(isNumber);
+console.log(typeof(isNumber)); */
+
+let someValue = "Lokesh"
+console.log(typeof(someValue));
+
+let converting = Number(someValue)
+console.log(converting);
+console.log(typeof(converting));
