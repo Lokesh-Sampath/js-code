c648b259bd7b1ca4cb1ff68398affed7e9ed4d02
Lokesh-Sampath <113461748+Lokesh-Sampath@users.noreply.github.com>
2023-10-28 13:50:07 +0000

    container config done

---
+++ .devcontainer/01_Basics/01_varaibles.js
@@ -0,0 +1,7 @@
+const accountId = 1174  // when we use const we will be not able to change the value after declaring it once
+let accountEmail = "loki@gmail.com"   // one of the most used type for declaring the varaibles
+var accountPassword = "29749274" // prefer not to use var, because issue in block and functional scope
+accountCity = "Chennai"  // we can declare some varaible just by their name, not using any keyword
+let accountState; // undefined varaible
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]); // table used to print the op in table form
+
