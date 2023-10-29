18b01d39fc9766245c5997905bb88308740544dd
Lokesh-Sampath <113461748+Lokesh-Sampath@users.noreply.github.com>
2023-10-30 01:01:58 +0530

    Add files via upload

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// alert methode can be used in browser only, not in node.js
+
+//ECMAScript for offical documentation-tc39.es
+
+let name = "Lokesh"  //string
+let age = 19        //number
+let isLoggedIn = false  //boolean
+let income  = null   // standalone value-empty value
+let state;           // undefined-unassinged value
+
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof income);
+console.log(typeof state);
+
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// type coversion basically 
+
+// "11"=>11
+//"11abc"=> NaN
+// true=>1  false=>0
+
+/*let score = "33"
+console.log(typeof(score));
+
+let isNumber  = Number(score)
+console.log(isNumber);
+console.log(typeof(isNumber)); */
+
+/*let someValue = "Lokesh"
+console.log(typeof(someValue));
+
+let converting = Number(someValue)
+console.log(converting);
+console.log(typeof(converting));*/
+
+
+//***************OPERAIONS************//
+
+
+/*let value = 3
+let negValue = -value // converts the value to negative of the original value
+console.log(negValue); */
+
+// string concatenation
+
+/*let str1 = "Lokesh"
+let str2 = " Hello"
+let str3 =  str2+str1
+
+console.log(str1+str2);
+console.log(str3);*/
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
